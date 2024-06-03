166b15e5775ef213e09e2acf25a972929994cc8e
Chqrety <fikriaverus23@gmail.com>
2024-06-03 13:32:34 +0700

    bulid: innitial commit

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div className="flex h-screen bg-[#373A40]">
+      <Link href="/profile" className="flex m-auto">
+        <div className="bg-gradient-to-br from-cyan-600 to-emerald-600 px-12 py-6 rounded-sm transition-all hover:scale-110 hover:bg-gradient-to-br hover:from-cyan-900 hover:to-emerald-900">
+          <span className="font-bold text-3xl">Go to profile</span>
+        </div>
+      </Link>
+    </div>
+  );
+}
